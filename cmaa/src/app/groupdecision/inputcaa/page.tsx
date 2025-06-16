2e2618363a0ac2e4d1f57e611d70a92d3c7ddc01
adriana <adrianad3m3@gmx.de>
2025-06-17 00:53:50 +0200

    implement saw solver, ranking, CMAA with Monte Carlo Simulation, rank acceptability counter

--- cmaa/src/app/groupdecision/inputcaa/page.tsx
+++ cmaa/src/app/groupdecision/inputcaa/page.tsx
@@ -1,4 +1,4 @@
- 'use client'
+'use client'
 
 import {GenericHeader} from "@/components/generics/GenericHeader";
 import {useSearchParams} from "next/navigation"; // useRouter hook should be imported from next/navigation
@@ -9,7 +9,7 @@ import {testData} from "@/components/cmaa/TestData";
 
 export default function CAAPage() {
 
-    // get data
+    // to get and store data
     const [decisionMakerData, setDecisionMakerData] = useState<{ [oid: string]: any }>({});
     const searchParams = useSearchParams()
 
@@ -19,6 +19,10 @@ export default function CAAPage() {
     // State to hold aggregated judgements
     const [aggregatedJudgements, setAggregatedJudgements] = useState<Set<number>[][]>([]);
 
+    // State to hold rank acceptability counter
+    const [rankAcceptabilityCounter, setRankAcceptabilityCounter] = useState<number[][]>([]);
+    // State to hold rank acceptability indices
+    const [rankAcceptabilityIndices, setRankAcceptabilityIndices] = useState<number[][]>([]);
 
     // Fetch and parse data from URL params
     useEffect(() => {
@@ -47,7 +51,7 @@ export default function CAAPage() {
                 // Generate data object for this key
                 if (typeof parsedData === 'object' && parsedData !== null) {
                     newData[key] = parsedData;
-                    console.log('new data:',newData);
+                    console.log('new data:', newData);
                 } else {
                     console.warn(`Parsed data for ${key} is not an object.`);
                 }
@@ -62,108 +66,13 @@ export default function CAAPage() {
             console.log('test Data:', fallbackData);
             setDecisionMakerData(fallbackData);
         } else {
-            console.log('Generated Data:', newData);
+            console.log('Generated Data from URL:', newData);
             setDecisionMakerData(newData);
         }
     }, [searchParams]);
 
-    /* example data object:
- {
-  "data": {
-    "1": [
-      {
-        "id": 0,
-        "criteria": "criterion 1",
-        "weight": 1,
-        "alt1": 2,
-        "alt2": 5,
-        "alt3": 1
-      },
-      {
-        "id": 1,
-        "criteria": "criterion 2",
-        "weight": 5,
-        "alt1": 3,
-        "alt2": 4,
-        "alt3": 2
-      },
-      {
-        "id": 2,
-        "criteria": "criterion 3",
-        "weight": 2,
-        "alt1": 4,
-        "alt2": 3,
-        "alt3": 3
-      },
-      {
-        "id": 1000,
-        "criteria": "weighted sum",
-        "alt1": 25,
-        "alt2": 31,
-        "alt3": 17,
-        "alt4": 0,
-        "alt5": 0
-      },
-      {
-        "id": 1001,
-        "criteria": "rank",
-        "alt1": 2,
-        "alt2": 1,
-        "alt3": 3,
-        "alt4": 4,
-        "alt5": 4
-      }
-    ],
-    "2": [
-      {
-        "id": 0,
-        "criteria": "criterion 1",
-        "weight": 4,
-        "alt1": 5,
-        "alt2": 1,
-        "alt3": 3
-      },
-      {
-        "id": 1,
-        "criteria": "criterion 2",
-        "weight": 2,
-        "alt1": 1,
-        "alt2": 2,
-        "alt3": 2
-      },
-      {
-        "id": 2,
-        "criteria": "criterion 3",
-        "weight": 3,
-        "alt1": 2,
-        "alt2": 5,
-        "alt3": 1
-      },
-      {
-        "id": 1000,
-        "criteria": "weighted sum",
-        "alt1": 28,
-        "alt2": 23,
-        "alt3": 19,
-        "alt4": 0,
-        "alt5": 0
-      },
-      {
-        "id": 1001,
-        "criteria": "rank",
-        "alt1": 1,
-        "alt2": 2,
-        "alt3": 3,
-        "alt4": 4,
-        "alt5": 4
-      }
-    ]
-  }
-}
-    * */
 
-
-    //function that generates aggregation of decisionMakerData
+    //function that generates aggregations of decisionMakerData
     // Run aggregation whenever decisionMakerData updates
     useEffect(() => {
         const aggPrefs: Set<number>[] = aggPreferences(decisionMakerData);
@@ -171,14 +80,19 @@ export default function CAAPage() {
         console.log('Aggregated Preferences:', aggPrefs);
 
         const aggJudgs: Set<number>[][] = aggJudgements(decisionMakerData);
-        setAggregatedJudgements(aggJudgements);
+        setAggregatedJudgements(aggJudgs);
         console.log('Aggregated Judgements:', aggJudgs);
+
+        const initialRACounter: number [][]= initializeRankAcceptabilityMatrix(decisionMakerData);
+        setRankAcceptabilityCounter(initialRACounter);
+        console.log('Initial RAC:', initialRACounter);
+
+        const initialRAIndices: number [][]= initializeRankAcceptabilityMatrix(decisionMakerData);
+        setRankAcceptabilityIndices(initialRAIndices);
+        console.log('Initial RAI:', initialRACounter);
+
     }, [decisionMakerData]);
 
-
-    //interface AggregatedPreferences {
-    //    Set<number>[]
-    //}
 
     //interface row object
     interface Row {
@@ -199,7 +113,6 @@ export default function CAAPage() {
         if (dmData.length === 0) {
             return aggPreferences;
         }
-
 
         // from the first decision maker
         const firstDM: Row[] = dmData[0][1]; // first array of row objects
@@ -295,7 +208,7 @@ export default function CAAPage() {
         */
     }
 
-    // aggJudgementMatrix ... Matrix for each criterion m and for each altn a set of unique values from decisionMaker judgements
+    // aggJudgementMatrix ... Matrix for each criterion m and for each alternative n - a set of unique values from decisionMaker judgements
 
     const aggJudgements = (data: { [oid: string]: any }): Set<number>[][] => {
         // matrix or 2-dimensional array of Set<number>
@@ -328,7 +241,7 @@ export default function CAAPage() {
             }
         });
 
-        console.log(criteriaArray, alternativesArray);
+        //console.log(criteriaArray, alternativesArray);
 
         const criteriaCount = criteriaArray.length;
         const alternativesCount = alternativesArray.length;
@@ -365,34 +278,202 @@ export default function CAAPage() {
         return aggJudgements;
     }
 
+    const initializeRankAcceptabilityMatrix = (data: { [oid: string]: any }): number[][] => {
+        // rows are implicitly a1...ai alternatives and cols are implicitly r1 ... rj ranks
+        let rankAcceptabilityMatrix: number[][] = [];
+
+        // check if data isEmpty -  data is object with oid --> that object then contains object arrays of decision maker data
+        const dmData = Object.values(data);
+
+        // error handling
+        if (dmData.length === 0) {
+            return rankAcceptabilityMatrix;
+        }
+        const firstRow: Row = dmData[0][1][0];
+        const alternativesArray: string[] = [];
+        // Extract keys starting with 'alt'
+        Object.keys(firstRow).forEach((key) => {
+            if (key.startsWith('alt')) {
+                alternativesArray.push(key);
+            }
+        });
+
+        // Matrix is ixj with i=j... number of alternatives
+        for (let i = 0; i < alternativesArray.length; i++) {
+            rankAcceptabilityMatrix[i] = [];
+            for (let j = 0; j < alternativesArray.length; j++) {
+                rankAcceptabilityMatrix[i][j] = 0;
+            }
+        }
+
+        return rankAcceptabilityMatrix;
+    }
+
+    /**
+     * Generate ranking based on weighted sum / performances of alternatives
+     * highest performance ... rank 1
+     */
+    const generateRanking = (preferences: number[], judgements: number [][]) => {
+
+        if (preferences.length === 0 || judgements.length === 0) {
+            return [];
+        }
+
+        // get performances of every alternative
+        const performances: number[] = generatePerformances(preferences, judgements);
+        //console.log('weighted sums: ', performances);
+
+        // Create an array of alternatives with their performances
+        const alternatives = performances.map((performance, index) => ({index, performance}));
+
+        // Sort alternatives in descending order of performance
+        alternatives.sort((a, b) => b.performance - a.performance);
+
+        // Extract sorted alternative indices as ranking
+        const rankings = alternatives.map(alternative => alternative.index+1);
+        //console.log('rankings ', rankings);
+
+        return rankings;
+
+    }
+
+    /**
+     * Simple additive weighting solver: returns weighted sums = performances of each alternative
+     */
+    const generatePerformances = (preferences: number[], judgements: number[][]): number[] => {
+        const performances: number[] = [];
+
+        // getting number of alternatives from first col
+        const numberAlts = judgements[0]?.length ?? 0; // assuming all criteria arrays are of same length
+        const numberCrits = judgements?.length ?? 0;
+
+        // iterate over cols = alternatives
+        for (let alternative = 0; alternative < numberAlts; alternative++) {
+            let sum = 0;
+            for (let criterion = 0; criterion < numberCrits; criterion++) {
+                sum += judgements[criterion][alternative] * preferences[criterion];
+            }
+            performances[alternative] = sum;
+        }
+
+        return performances;
+    }
+    const updateCounters = (preferences: number[], judgements: number[][], ranks: number[]) => {
+        const rankAccs: number[][] = rankAcceptabilityCounter;
+        //console.log('rankAccs', rankAccs);
+
+        let rank: number = 0;
+
+        for (let alternative = 0; alternative < ranks.length; alternative++) {
+            rank = ranks[alternative] - 1; // rank-1 is col index in rankAccs
+            rankAccs[alternative][rank] += 1;
+        }
+        setRankAcceptabilityCounter(rankAccs)
+    }
+
+    // use randomNumberGenerator with uniform chance for Monte Carlo simulations
+
+    // UseRef to hold the seedrandom generator instance
     const rngRef = useRef<seedrandom.PRNG | null>(null);
 
     // Initialize the seedrandom generator once
-    const initializeGenerator = () => {
-        rngRef.current = seedrandom('1234');
+    const initializeGenerator = (seed?: string) => {
+        //fallback seed
+        const superSeed: string = seed ?? '1234';
+        rngRef.current = seedrandom(superSeed);
     };
 
-    // Generate a random number between 0 and 1
-    const getRandomNumber = () => {
+    // Generate random number between 0 and 1
+    const getRandomNumber = (): number => {
         if (rngRef.current) {
             return rngRef.current();
         }
-        return null;
+        // Fallback if RNG isn't initialized
+        console.warn('RNG not initialized!');
+        return Math.random(); // fallback to Math.random() as a safe default
     };
 
     React.useEffect(() => {
-        initializeGenerator();
-        console.log('Random number:', getRandomNumber());
+        initializeGenerator(); // CHANGE SEED HERE
+        //console.log('Random number:', getRandomNumber());
     }, []);
+
+    const generateRandomInstance = (aggrPreferences: Set<number>[], aggrJudgements: Set<number>[][], seed?: string) => {
+        // Initialize generator if seed provided, or keep existing
+        if (seed) {
+            initializeGenerator(seed);
+        }
+
+        // generate deterministic random preferences
+        const preferences = aggrPreferences.map(() => {
+            return Math.floor(getRandomNumber() * 9) + 1; // 1 to 9 : CHANGE RANGE HERE
+        });
+
+        //console.log('rows number', aggrJudgements.length)
+        //console.log('cols number', aggrJudgements[1].length)
+
+        // initiate judgments matrix
+        let judgements: number[][] = [];
+        for (let i = 0; i < aggrJudgements.length; i++) {
+            judgements[i] = [];
+            for (let j = 0; j < aggrJudgements[i].length; j++) {
+                judgements[i][j] = 0;
+            }
+        }
+
+        // generate values from existing sets
+        for (let i = 0; i < aggrJudgements.length; i++) {
+            const row = aggrJudgements[i];
+            for (let j = 0; j < aggrJudgements[i].length; j++) {
+                const arr: number[] = Array.from(row[j]);
+                //console.log(arr);
+                // index in range of numbers from set
+                const randIdx: number = Math.floor(getRandomNumber() * arr.length);
+                judgements[i][j] = arr[randIdx];
+            }
+        }
+
+        //console.log('random instance: ', preferences, judgements);
+        return {preferences, judgements};
+    }
+
+    const createStatistics = () => {
+      
+    }
+
+    function cMAA() {
+
+        if (Object.values(decisionMakerData).length === 0) {
+            return;
+        }
+
+        const kMonteCarlo = 10000;
+        for (let k = 0; k < kMonteCarlo; k++) {
+            const instance = generateRandomInstance(aggregatedPreferences, aggregatedJudgements);
+            const ranks = generateRanking(instance.preferences, instance.judgements);
+            updateCounters(instance.preferences, instance.judgements, ranks);
+        }
+
+        console.log('rankAcceptabilityIndx ', rankAcceptabilityCounter)
+    }
+
+    // execute CMAA algorithm when all parameters change
+    useEffect(() => {
+        //console.log('cMAA');
+        if ( // check if necessary data defined
+            Object.keys(decisionMakerData) !== undefined &&
+            aggregatedPreferences !== undefined &&
+            aggregatedJudgements !== undefined && aggregatedJudgements[0] !== undefined
+        ) {
+            //console.log('cMAA iteration');
+            cMAA();
+        }
+    }, [decisionMakerData, aggregatedPreferences, aggregatedJudgements]);
 
     return (
         <Grid>
             <GenericHeader title={'Schritt 2: Combinatorial Acceptability Analysis'}/>
             <Grid>
-                <Button onClick={() => {
-                    initializeGenerator();
-                    alert(`New seed random number: ${getRandomNumber()}`);
-                }} variant={"contained"}>random</Button>
             </Grid>
         </Grid>
     )
--- cmaa/src/components/cmaa/TestData.tsx
+++ cmaa/src/components/cmaa/TestData.tsx
@@ -1,5 +1,6 @@
 export const testData = () => {
-    const rawData = {
+
+    return {
         0: {
             "1": [
                 {
@@ -176,188 +177,99 @@ export const testData = () => {
         }
 
     }
-
-    // Convert data object to an array of rows
-    //const resultArray = Object.values(rawData.data).flat();
-
-    return {
-                0: {
-            "1": [
-                {
-                    id: 0,
-                    criteria: "criterion 1",
-                    weight: 5,
-                    alt1: 9,
-                    alt2: 5,
-                    alt3: 5,
-                    alt4: 3,
-                    alt5: 7,
-                    alt6: 9,
-                },
-                {
-                    id: 1,
-                    criteria: "criterion 2",
-                    weight: 9,
-                    alt1: 9,
-                    alt2: 5,
-                    alt3: 9,
-                    alt4: 7,
-                    alt5: 3,
-                    alt6: 9,
-                },
-                {
-                    id: 2,
-                    criteria: "criterion 3",
-                    weight: 7,
-                    alt1: 7,
-                    alt2: 7,
-                    alt3: 5,
-                    alt4: 7,
-                    alt5: 3,
-                    alt6: 3,
-                },
-                {
-                    id: 3,
-                    criteria: "criterion 4",
-                    weight: 3,
-                    alt1: 9,
-                    alt2: 7,
-                    alt3: 7,
-                    alt4: 7,
-                    alt5: 7,
-                    alt6: 1,
-                },
-                {
-                    id: 4,
-                    criteria: "criterion 5",
-                    weight: 5,
-                    alt1: 7,
-                    alt2: 3,
-                    alt3: 3,
-                    alt4: 7,
-                    alt5: 5,
-                    alt6: 5,
-                }
-            ],
-            "2": [
-                {
-                    id: 0,
-                    criteria: "criterion 1",
-                    weight: 3,
-                    alt1: 9,
-                    alt2: 3,
-                    alt3: 3,
-                    alt4: 7,
-                    alt5: 5,
-                    alt6: 1,
-                },
-                {
-                    id: 1,
-                    criteria: "criterion 2",
-                    weight: 7,
-                    alt1: 1,
-                    alt2: 9,
-                    alt3: 3,
-                    alt4: 5,
-                    alt5: 5,
-                    alt6: 1,
-                },
-                {
-                    id: 2,
-                    criteria: "criterion 3",
-                    weight: 9,
-                    alt1: 3,
-                    alt2: 3,
-                    alt3: 9,
-                    alt4: 1,
-                    alt5: 5,
-                    alt6: 9,
-                },
-                {
-                    id: 3,
-                    criteria: "criterion 4",
-                    weight: 5,
-                    alt1: 9,
-                    alt2: 5,
-                    alt3: 5,
-                    alt4: 7,
-                    alt5: 5,
-                    alt6: 7,
-                },
-                {
-                    id: 4,
-                    criteria: "criterion 5",
-                    weight: 5,
-                    alt1: 1,
-                    alt2: 9,
-                    alt3: 3,
-                    alt4: 9,
-                    alt5: 5,
-                    alt6: 9,
-                }
-            ],
-            "3": [
-                {
-                    id: 0,
-                    criteria: "criterion 1",
-                    weight: 5,
-                    alt1: 9,
-                    alt2: 3,
-                    alt3: 3,
-                    alt4: 7,
-                    alt5: 3,
-                    alt6: 3,
-                },
-                {
-                    id: 1,
-                    criteria: "criterion 2",
-                    weight: 7,
-                    alt1: 5,
-                    alt2: 3,
-                    alt3: 9,
-                    alt4: 5,
-                    alt5: 7,
-                    alt6: 9,
-                },
-                {
-                    id: 2,
-                    criteria: "criterion 3",
-                    weight: 7,
-                    alt1: 3,
-                    alt2: 3,
-                    alt3: 5,
-                    alt4: 5,
-                    alt5: 5,
-                    alt6: 7,
-                },
-                {
-                    id: 3,
-                    criteria: "criterion 4",
-                    weight: 5,
-                    alt1: 7,
-                    alt2: 3,
-                    alt3: 5,
-                    alt4: 7,
-                    alt5: 9,
-                    alt6: 3,
-                },
-                {
-                    id: 4,
-                    criteria: "criterion 5",
-                    weight: 9,
-                    alt1: 5,
-                    alt2: 3,
-                    alt3: 7,
-                    alt4: 5,
-                    alt5: 7,
-                    alt6: 3,
-                }
-            ]
-
-        }
-        /*Object: {
-            data: resultArray
-        }
-         */
-    }
 };
+
+   /* example data object:
+ {
+  "data": {
+    "1": [
+      {
+        "id": 0,
+        "criteria": "criterion 1",
+        "weight": 1,
+        "alt1": 2,
+        "alt2": 5,
+        "alt3": 1
+      },
+      {
+        "id": 1,
+        "criteria": "criterion 2",
+        "weight": 5,
+        "alt1": 3,
+        "alt2": 4,
+        "alt3": 2
+      },
+      {
+        "id": 2,
+        "criteria": "criterion 3",
+        "weight": 2,
+        "alt1": 4,
+        "alt2": 3,
+        "alt3": 3
+      },
+      {
+        "id": 1000,
+        "criteria": "weighted sum",
+        "alt1": 25,
+        "alt2": 31,
+        "alt3": 17,
+        "alt4": 0,
+        "alt5": 0
+      },
+      {
+        "id": 1001,
+        "criteria": "rank",
+        "alt1": 2,
+        "alt2": 1,
+        "alt3": 3,
+        "alt4": 4,
+        "alt5": 4
+      }
+    ],
+    "2": [
+      {
+        "id": 0,
+        "criteria": "criterion 1",
+        "weight": 4,
+        "alt1": 5,
+        "alt2": 1,
+        "alt3": 3
+      },
+      {
+        "id": 1,
+        "criteria": "criterion 2",
+        "weight": 2,
+        "alt1": 1,
+        "alt2": 2,
+        "alt3": 2
+      },
+      {
+        "id": 2,
+        "criteria": "criterion 3",
+        "weight": 3,
+        "alt1": 2,
+        "alt2": 5,
+        "alt3": 1
+      },
+      {
+        "id": 1000,
+        "criteria": "weighted sum",
+        "alt1": 28,
+        "alt2": 23,
+        "alt3": 19,
+        "alt4": 0,
+        "alt5": 0
+      },
+      {
+        "id": 1001,
+        "criteria": "rank",
+        "alt1": 1,
+        "alt2": 2,
+        "alt3": 3,
+        "alt4": 4,
+        "alt5": 4
+      }
+    ]
+  }
+}
+    * */
